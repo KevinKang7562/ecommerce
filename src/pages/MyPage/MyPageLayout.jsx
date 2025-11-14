888bb67a2cecb37fd36178b5de8693ffd75e281c
jh <awg3200@gmail.com>
2025-11-14 17:39:54 +0900

    불필요한 주석 제거

--- src/pages/MyPage/MyPageLayout.jsx
+++ src/pages/MyPage/MyPageLayout.jsx
@@ -1,29 +1,4 @@
 //마이페이지 기본 레이아웃
-// import { Offline } from 'react-detect-offline';
-// import { Outlet } from 'react-router-dom';
-// import MyPageSidebar from './MyPageSidebar';
-
-// export default function MyPageLayout() {
-//   return (
-//     <div className="flex flex-col min-h-screen">
-//       <Offline>
-//         <p className="w-fit font-bold bg-red-500 rounded-lg text-white text-center p-2 fixed bottom-5 right-5 z-10">
-//           인터넷 연결이 끊어졌습니다. 네트워크를 확인해주세요.
-//         </p>
-//       </Offline>
-//       <div className="flex min-h-screen bg-gray-50">
-//         {/* 왼쪽 사이드바 */}
-//         <MyPageSidebar />
-
-//         {/* 오른쪽 본문 영역 */}
-//         <main className="flex-1 p-6">
-//           <Outlet /> {/* 자식 라우트들(MyProfileEdit 등) 렌더링 */}
-//         </main>
-//       </div>
-//     </div>
-//   );
-// }
-
 import { Outlet, useLocation } from 'react-router-dom';
 import MyPageSidebar from './MyPageSidebar';
 import { myPageMenus } from './menuData';
