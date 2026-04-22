e7f4e36cf90c6bd0cf57789e3e142e844f1ef95b
jh <awg3200@gmail.com>
2026-04-22 15:49:07 +0900

    DEFAULT_EVENT_IMAGE 추가

--- src/constants/api.js
+++ src/constants/api.js
@@ -6,6 +6,7 @@ export const IMAGE_BASE_URL =
   import.meta.env.VITE_IMAGE_BASE_URL || 'http://192.168.0.225'; //사무실 이전으로 아이피 변경
 
 export const DEFAULT_PRODUCT_IMAGE = `${IMAGE_BASE_URL}/zin_user1/productImg/default_product_image.png`;
+export const DEFAULT_EVENT_IMAGE = `${IMAGE_BASE_URL}/zin_user1/productImg/default_event_image.png`;
 
 const trimTrailingSlash = (value) => value.replace(/\/+$/, '');
 
