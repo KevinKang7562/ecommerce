4d54efa2b5e159bc56d5a50952c4f585a315f6ef
jh <awg3200@gmail.com>
2026-04-22 23:57:40 +0900

    Spinner 컴포넌트 스타일 수정

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,11 +1,17 @@
 import style from './Spinner.module.css';
 
 export default function Spinner() {
+  // return (
+  //   <div className="fixed inset-0 flex items-center justify-center bg-white/50 z-50">
+  //     <div className={`flex justify-center items-center`}>
+  //       <span className={style.loader}></span>
+  //     </div>
+  //   </div>
+  // );
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-white/50 z-50">
-      <div className={`flex justify-center items-center`}>
-        <span className={style.loader}></span>
-      </div>
+    // 💡 w-full(너비 100%), h-full(높이 100%), min-h-[200px](영역이 좁을 때를 대비한 최소 높이 200px 보장)
+    <div className="flex justify-center items-center w-full h-full min-h-[200px]">
+      <span className={style.loader}></span>
     </div>
   );
 }
